3e76681461c4ef28d3d480d6440e2f314aac7a55
Danieltc14 <147975800+Danieltc14@users.noreply.github.com>
2025-10-02 14:58:46 +0100

    Update Unit_test.py

--- Unit_test.py
+++ Unit_test.py
@@ -4,7 +4,7 @@ from function import add
 class TestFunction(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(-1, 1), 0)
-        self.assertEqual(add(0, 0), 0)
+        self.assertEqual(add(1, 0), 1)
         self.assertEqual(add(-3, -7), -4)
 
 if __name__ == "__main__":
