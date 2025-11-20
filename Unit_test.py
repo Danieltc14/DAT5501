35aaeab61bb5426e2c9a6a71b9a8b6b60cb584a7
Danieltc14 <danielcoker8101@icloud.com>
2025-11-20 11:16:59 +0000

    Commiting Unit_test.py and wages.py

--- Unit_test.py
+++ Unit_test.py
@@ -3,9 +3,8 @@ from function import newadd
 
 class TestFunction(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(-1, 1), 0)
-        self.assertEqual(add(1, 0), 1)
-        self.assertEqual(add(-3, -7), -4)
-
+        self.assertEqual(newadd(-1, 1), 0)
+        self.assertEqual(newadd(1, 0), 1)
+        
 if __name__ == "__main__":
     unittest.main()
---
+++ wages.py
@@ -0,0 +1,3 @@
+import numpy as np
+# example: fit a polynomial of order n
+z = np.polyfit(sub_sample_years, sub_sample_values, n)
